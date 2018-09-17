ba3fb7ba8b4a72aa7e14f65bad580643499ae8ba
Leibold.Kai <Leibold.Kai@SK-448A5BCB8A89.2631.skool.net>
2018-09-17 15:31:13 +0200

    lel

---
+++ rong/src/rong/duy.java
@@ -0,0 +1,21 @@
+package rong;
+
+public class duy {
+
+	
+	public static void main(String args[]){
+		
+		int zahl = 7;
+	while(zahl>=1){	
+		
+		if(zahl % 2 != 0){
+			zahl=zahl*3+1;
+			}
+		else{zahl = zahl/2;}
+	
+		System.out.print(zahl + ", ");
+		}
+	
+	}
+}
+
