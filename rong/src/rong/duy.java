454829bfc0e1e9b3c7af6f9a46dc4bed61ba19e0
Leibold.Kai <Leibold.Kai@192.168.9.18>
2018-09-20 08:12:32 +0200

    collatz

--- rong/src/rong/duy.java
+++ rong/src/rong/duy.java
@@ -4,9 +4,25 @@ public class duy {
 
 	
 	public static void main(String args[]){
+
+	
+	int zahl;     // Initialisierung der Variable, in der die Collatzberechnung stattfindet 
+	
+	/* 789
+	 * 456
+	 * 123
+	 * *0#
+	 */
+	
+	
+	
+	for(int i = 5; i <= 100;i++){
+		zahl=i;
+		System.out.print("Seed: "+i+"| ");
 		
-		int zahl = 7;
-	while(zahl>=1){	
+	while(zahl>1){	
+		
+			
 		
 		if(zahl % 2 != 0){
 			zahl=zahl*3+1;
@@ -14,8 +30,10 @@ public class duy {
 		else{zahl = zahl/2;}
 	
 		System.out.print(zahl + ", ");
+		//System.out.print(i + ", ");
 		}
+	    System.out.println("");
 	
+		}
 	}
 }
-
