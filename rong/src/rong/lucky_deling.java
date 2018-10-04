227481f2d570ff4d133edacb9f14a46dd89d82e5
Leibold.Kai <Leibold.Kai@DESKTOP-MJNLJH0.2631.skool.net>
2018-10-04 08:19:46 +0200

    Gluecksspiel

---
+++ rong/src/rong/lucky_deling.java
@@ -0,0 +1,40 @@
+package rong;
+
+public class lucky_deling {
+
+	
+	
+	public static void main(String[] args) {
+
+	
+		 boolean geschlecht  = true ;
+		 int alter = 15;
+		 String name = "Susanne" ;
+		
+		if(alter<18){
+			System.out.println("Hallo "+name);
+			System.out.println("Du hast "+wuerfeln()+"!");
+		}
+		else if(!geschlecht){
+			System.out.println("Lieber Herr "+ name+"!");
+			System.out.println("Du hast "+ wuerfeln +"!");
+			
+		}
+	}	
+		
+	public static String wuerfeln(){
+		if(Math.random()<=0.42){
+			return("Verloren!");
+		}
+		else{return("Gewonnen!");}
+		
+	}
+	
+			
+		
+		
+		
+		
+}
+
+
